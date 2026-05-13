51f44ec20e7b263c85cfc91b6788a82dc70ecb3f
TonyWu20 <tony.w21@gmail.com>
2026-05-13 15:15:27 +0800

    chore: add .gitignore for lsp-workspace target artifacts EOF )

--- tests/fixtures/lsp-workspace/target/debug/deps/lsp_fixture-ab5c013ca3394e07.d
+++
@@ -1,5 +0,0 @@
-/Users/tony/programming/wait-for-lsp/tests/fixtures/lsp-workspace/target/debug/deps/lsp_fixture-ab5c013ca3394e07.d: src/main.rs
-
-/Users/tony/programming/wait-for-lsp/tests/fixtures/lsp-workspace/target/debug/deps/liblsp_fixture-ab5c013ca3394e07.rmeta: src/main.rs
-
-src/main.rs:
